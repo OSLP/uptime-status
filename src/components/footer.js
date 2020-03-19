5c7f781ae5cd103d0908f29b263578e7b260c544
Shiroka Arukas <h@xuexi.icu>
2020-03-19 12:22:45 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="container">
-        <p>Powered By <Link to="https://uptimerobot.com/" text="UptimeRobot" /> API|Check every 5 min</p>
+        <p>Powered By <Link to="https://uptimerobot.com/" text="UptimeRobot" /> API | Check every 5 min</p>
         <p>&copy; 2020 <Link to="https://jaw.jp" text="株式会社JAW" /></p>
       </div>
     </div>
